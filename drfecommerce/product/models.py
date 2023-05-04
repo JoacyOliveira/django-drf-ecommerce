e58096e5a3b48e4a2326847fd6c42bf7376d100f
Joacy <thesoldier100052@gmail.com>
2023-05-04 20:56:13 -0300

    Criado os serializers

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -4,22 +4,23 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 class Category(MPTTModel):
     name = models.CharField(max_length=100)
-    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+    parent = TreeForeignKey(
+        "self", on_delete=models.PROTECT, null=True, blank=True)
 
     class MPTTMeta:
         order_insertion_by = ["name"]
 
     def __str__(self):
         return self.name
-    
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
-    
-    
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
@@ -31,4 +32,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
